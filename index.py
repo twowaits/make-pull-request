d6b75bb83a3cc0eb6ccf6a88bef5db458367c2ce
A V S ADITYAVARDHAN <47914143+AVS18@users.noreply.github.com>
2020-10-01 09:08:03 +0530

    Factorial

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+n = int(input())
+print(n**2)
