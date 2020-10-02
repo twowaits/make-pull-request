6fbe547cf828340d312ab9b4658331a8272d4229
anuragchaurasia3108 <71565271+anuragchaurasia3108@users.noreply.github.com>
2020-10-02 05:04:27 -0700

    Fixing the issue of input user details
    simply I remove the text of hello world and take  all input details then display all details in well formatted way...

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+fullName =input("enter your fullName:- ")
+fatherName = input("enter your fatherName:- ")
+date = int(input("enter your birthDate:- "))
+month = int(input("enter your birthMonth:- "))
+year = int(input("enter your birthYear:- "))
+address = input("enter your fullAddress:- ")
+print(f"Your Name is: {fullName}\nYour Father Name: {fatherName}\n Date Of Birth: {date}-{month}-{year}\nFulladdress: {address}")
