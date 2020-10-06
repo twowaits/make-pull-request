b1893adb46467e697fd153c9d1ebf07228503b09
Ishika Gupta <guptaishika172@gmail.com>
2020-10-06 19:14:30 +0530

    6thOct-7.py

--- Python/6thOct-7.py
+++ Python/6thOct-7.py
@@ -1,30 +1,32 @@
 def fibo(n):
-			l=[1]
-			a = 0
-			b = 1
-			if n <= 0:
-				print("Enter the Integer")
-			elif n == 1:
-				print(b) 
-			else:
-				for i in range(1,n):
-					sum = a + b
-					a = b
-					b = sum
-					if b%5==0 or Prime(b):
-						l.append(0)
-					else:
-						l.append(b)
-				print(l) 
+	l=[1]
+	a = 0
+	b = 1
+	  if n <= 0:
+	    print("Enter the Integer")
+	  elif n == 1:
+        	print(b) 
+	  else:
+              for i in range(1,n):
+		sum = a + b
+		a = b
+		b = sum
+	
+	if b%5==0 or Prime(b):
+					
+	   l.append(0)
+	else:
+           l.append(b)
+	    print(l) 
 		
-		def Prime(num):  
-		        if num > 1:
-		           for i in range(2,num):
-		               if (num % i) == 0:
-		                   return False
-		           else:
-		               return True
-		        else:
-		           return False
+	def Prime(num):  
+          if num > 1:
+            for i in range(2,num):
+	       if (num % i) == 0:
+	         return False
+	       else:
+	         return True
+	       else:
+	         return False
 		
-		fibo(int(input("Enter Number Of terms: ")))
+fibo(int(input("Enter Number Of terms: ")))
