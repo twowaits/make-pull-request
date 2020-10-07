54b6545e36f243967cd7e6d408de5e47ceb1599f
Anant Agnihotri <57098229+anant2207@users.noreply.github.com>
2020-10-07 19:46:32 +0530

    Update 7th-Oct.py
    Corrected the mistakes.

--- Python/7th-Oct.py
+++ Python/7th-Oct.py
@@ -2,22 +2,40 @@ def number(num):
 	s=str(num)
 	if len(s)==1:
 		print(num)
+		
 	elif len(s)==2:
-		print(s[-2],"Ten",s[-1])
+	    if(s[-2]=="1"):
+	        print(s[-2],"Ten",s[-1])
+	    else:
+		    print(s[-2],"Tens",s[-1])
+	
 	elif len(s)==3:
-		print(s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    if(s[-3]=="1"):
+	        print(s[-3],"Hundred",s[-2],"Tens and",s[-1])
+	    else:
+		    print(s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	
 	elif len(s)==4:
-		print(s[-4],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    if(s[-4]=="1"):
+	        print(s[-4],"Thousand",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    else:
+		    print(s[-4],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	
 	elif len(s)==5:
 		print(s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	
 	elif len(s)==6:
-		print(s[-6],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
-	elif len(s)==6:
-		print(s[-6:-3],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    if(s[-6]=="1"):
+		    print(s[-6],"Lac",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    else:
+		    print(s[-6],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
 	elif len(s)==7:
 		print(s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
 	else:
-		print(s[:-7],"Crores",s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    if(s[-8]=="1"):
+		    print(s[:-7],"Crore",s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+	    else:
+		    print(s[:-7],"Crores",s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
 
 
 n=int(input("Enter the Number : "))
