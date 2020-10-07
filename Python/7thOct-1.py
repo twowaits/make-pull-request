562a3db9dbc63d7e85f5753541e4fc0c6612ead5
Amit Kumar Yadav <amit.ydv055@gmail.com>
2020-10-07 18:06:58 +0545

    Edit 7thOct-1.py
    Edited 7thOct-1.py to match sample I/O.

--- Python/7thOct-1.py
+++ Python/7thOct-1.py
@@ -3,19 +3,19 @@ def numberToWords(n):
 	if len(s)==1:
 		print(n)
 	elif len(s)==2:
-		print(s[-2],"Tens",s[-1])
+		print(s[-2],"tens",s[-1])
 	elif len(s)==3:
-		print(s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[-3],"hundreds",s[-2],"tens and",s[-1])
 	elif len(s)==4:
-		print(s[-4],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[-4],"thousands",s[-3],"hundreds",s[-2],"tens and",s[-1])
 	elif len(s)==5:
-		print(s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[-5:-3],"thousands",s[-3],"hundreds",s[-2],"tens and",s[-1])
 	elif len(s)==6:
-		print(s[-6],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[-6],"lacs",s[-5:-3],"thousands",s[-3],"hundreds",s[-2],"tens and",s[-1])
 	elif len(s)==7:
-		print(s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[-7:-5],"lacs",s[-5:-3],"thousands",s[-3],"hundreds",s[-2],"tens and",s[-1])
 	else:
-		print(s[:-7],"Crores",s[-7:-5],"Lacs",s[-5:-3],"Thousands",s[-3],"Hundreds",s[-2],"Tens and",s[-1])
+		print(s[:-7],"crores",s[-7:-5],"lacs",s[-5:-3],"thousands",s[-3],"hundreds",s[-2],"tens and",s[-1])
 
 
 try:
