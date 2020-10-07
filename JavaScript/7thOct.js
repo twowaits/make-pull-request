8fb92bb05935c94d898de784c1fec59d32e67862
Ayush Mishra <72432876+ayush-mishra-git@users.noreply.github.com>
2020-10-07 14:06:10 +0530

    7thOct.js

---
+++ JavaScript/7thOct.js
@@ -0,0 +1,39 @@
+var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+
+ 
+
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+
+
+
+function inWords (num) {
+
+    if ((num = num.toString()).length > 9) return 'overflow';
+
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+
+    if (!n) return; var str = '';
+
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+
+    return str;
+
+}
+
+
+
+
+document.getElementById('number').onkeyup = function () {
+
+    document.getElementById('words').innerHTML = inWords(document.getElementById('number').value);
+
+};
