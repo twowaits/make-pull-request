b45507d47592ffe99324b6d642a7925d9390c230
ekanshverma5 <60564078+ekanshverma5@users.noreply.github.com>
2020-10-08 21:12:23 +0530

    Add files via upload
    Hollow_Pyramid

--- Python/8thOct.py
+++ Python/8thOct.py
@@ -1,20 +1,8 @@
-# Print Hollow Pyramid Pattern Using Stars
-
-def hollowPyramid(r):
-    for i in range(r):
-        for j in range(r-i):
-            print(' ', end='') # print required space while staying in same line
-
-        for j in range(2*i+1):
-            if j==0 or j==2*i or i==r-1:
-                print('*',end='')
-            else:
-                print(' ', end='')
-        print() # print new line
-
-try:
-    row = int(input('Enter number of rows: '))
-    hollowPyramid(row)
-
-except:
-    print("Please re-run the program and enter valid number.")
+N=int(input())
+for i in range(1,N+1):
+    for j in range(1,2*N):
+        if i==N or i+j==N+1 or j-i==N-1:
+          print("*",end="")
+        else:
+          print(end=" ")
+    print()
