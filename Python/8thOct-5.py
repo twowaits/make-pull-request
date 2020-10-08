972e35ed034b0d00b9bf6c4d695bd872971339cd
Swayamsidh Nayak <58362354+swayam50@users.noreply.github.com>
2020-10-08 15:27:06 +0530

    Hollow Star using Python PR

---
+++ Python/8thOct-5.py
@@ -0,0 +1,11 @@
+n = int (input())
+
+gap = 1
+for i in range(1,n+1):
+	if i == 1 :
+		print ("*".center(n*2 - 1))
+	elif i != n:
+		print (((" "*gap).join("*"*2)).center(n*2 -1))
+		gap += 2
+	else:
+		print (" ".join("*"*n))
