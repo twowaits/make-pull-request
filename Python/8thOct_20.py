dd0004e96bc04ea3784f5efa5ab1e1bfa81565fe
Chaitali3005 <narkhede.chaitali@gmail.com>
2020-10-08 19:16:52 +0530

    Added Python code for Hollow Triangle

---
+++ Python/8thOct_20.py
@@ -0,0 +1,28 @@
+# Python program to print a hollow  
+# pyramid pattern 
+  
+def printPattern( n) : 
+    k = 0
+    for i in range(1,n+1) : #row 6 
+      
+        # Print spaces 
+        for j in range(i,n) : 
+            print(' ', end='') 
+          
+        # Print # 
+        while (k != (2 * i - 1)) : 
+            if (k == 0 or k == 2 * i - 2) : 
+                print('#', end='') 
+            else : 
+                print(' ', end ='') 
+            k = k + 1
+        k = 0; 
+        print ("") # print next row 
+          
+    # print last row 
+    for i in range(0, 2 * n -1) : 
+        print ('*', end = '') 
+  
+# Driver code  
+n = 6
+printPattern(n) 
