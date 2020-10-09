92a2c10c31d6be575eb185224acf9871e8d6bb9c
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-10-09 18:03:09 +0530

    Update 9oct1.py

--- Python/9oct1.py
+++ Python/9oct1.py
@@ -1,13 +1,7 @@
-gcd = 0
-print(“Enter two numbers : “)
-a = int(input())
-b = int(input())
-i = 1
-     while(i <= a and i <= b):
-          if((a % i == 0) and (b % i == 0)):
-               gcd = i
-          i = i + 1
-
-print(“GCD : “,end=”);
-print(gcd)
-
+import math 
+  
+def gcdOfFactorial(m, n) : 
+    return math.factorial(min(m, n)) 
+  
+n = int(input("Enter an number >>>"))
+m = int(input("Enter an number >>>"))
