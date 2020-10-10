2ea240ed2b17c35334826ed421e90e5e066cbd5d
YUGANSHCHAUHAN <70768923+YUGANSHCHAUHAN@users.noreply.github.com>
2020-10-10 17:20:06 +0530

    Update 10th0ct-4.py

--- Python/10th0ct-4.py
+++ Python/10th0ct-4.py
@@ -1,6 +1,6 @@
 def rev(a,b):
     print(a[::-1])
-    print(b[::-1])
-name=input("enter your name:")
-surname=input("enter your surname:")
-rev(name,surname)
+    
+a=input("enter your name:")
+
+rev(a)
