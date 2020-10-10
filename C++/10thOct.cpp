d1287f051305801c4dd5376b29d78be3e97e02c3
Ravi Gupta <61700885+ravi9607@users.noreply.github.com>
2020-10-10 18:26:11 +0530

    10thOct.cpp

---
+++ C++/10thOct.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void reverseStr(string& str) 
+{ 
+	int n = str.length(); 
+ 
+	for (int i = 0; i < n / 2; i++) 
+		swap(str[i], str[n - i - 1]); 
+} 
+
+int main() 
+{ 
+	string str = "geeksforgeeks"; 
+	reverseStr(str); 
+	cout << str; 
+	return 0; 
+} 
