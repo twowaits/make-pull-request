616d884dc693f4a637bacb81fdd25a38f62ccb38
Shreya <52084473+sshreya2912@users.noreply.github.com>
2020-10-10 14:01:54 +0530

    10thOct.cpp
    A program to reverse the given strings
    -by Shreya
    ABES Engineering College

---
+++ C++/10thOct.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+{ 
+string str;
+while(cin>>str)
+{  
+
+reverse(str.begin(),str.end());
+cout<<str<<endl;
+  
+    
+  }
+    return 0; 
+} 
