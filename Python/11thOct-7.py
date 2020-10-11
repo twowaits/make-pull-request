7d5b153e153d96b6a2e30fb8451431acc6517dd3
Yash <yashrathor5015@gmail.com>
2020-10-11 16:24:09 +0530

    11thOct.py

---
+++ Python/11thOct-7.py
@@ -0,0 +1,18 @@
+def feb(n):
+    a=1
+    b=1
+    if n==1:
+        print(a)
+    elif n==2:
+        print(a,b)
+    else:
+        print(a,b,end=" ")
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+            print(b,end=" ")
+n=int(input("Enter number: "))
+feb(n)
+
+
