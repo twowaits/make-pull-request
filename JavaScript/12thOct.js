3f14b51b00c82da138b5d0fcff123cb3bacd49c1
Anand Mohan <51752139+anandmohan23@users.noreply.github.com>
2020-10-12 13:45:47 +0530

    Add 12thOct.js

---
+++ JavaScript/12thOct.js
@@ -0,0 +1,8 @@
+<script>
+    const num = [0, 1, 2, 0, 1, 2];
+    const num2 = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1];
+    num.sort();
+    num2.sort(); 
+    console.log(num);
+    console.log(num2);
+</script>
