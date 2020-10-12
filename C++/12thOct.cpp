65b76358f9811b3c5a7d82d61416c5dee44543a9
Sumit Puri <69988229+Sumit-Puri@users.noreply.github.com>
2020-10-12 13:03:11 +0530

    Added 12thOct.cpp

---
+++ C++/12thOct.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <algorithm> 
+
+using namespace std; 
+
+void show(int a[]) 
+{ 
+} 
+
+int main() 
+{ 
+	int a[100],i=0;
+	while(cin>>a[i])
+	{i++;
+	}
+
+	sort(a, a+i); 
+
+	for(int j = 0; j <i ; ++j) 
+		cout << a[j] << " "; 
+	return 0; 
+
+} 
