a6bb834fdeb78f675b6d3aae19f3ae6511181f13
Megha gupta <46380159+gmegha12@users.noreply.github.com>
2020-10-12 21:07:21 +0530

    Added 12thOct-2.py

---
+++ Python/12thOct-2.py
@@ -0,0 +1,18 @@
+a=list(map(int,input().split()))
+count_0=0
+count_1=0
+n=0
+for i in range(len(a)):
+    if a[i]==0:
+        n=a.pop(i)
+        a.insert(0,n)
+        count_0=count_0+1
+    elif a[i]==1:
+        n=a.pop(i)
+        a.insert(count_0,n)
+        count_1=count_1+1
+    elif a[i]==2:
+        n=a.pop(i)
+        a.insert(count_1+count_0,n)
+        
+print(a)
