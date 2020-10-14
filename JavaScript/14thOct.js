051d329efcf72c406281de8cfeb6015d09bac416
Saurabh-172 <saurabh300.gole@gmail.com>
2020-10-15 00:01:32 +0530

    update 14thOct.js

--- JavaScript/14thOct.js
+++ JavaScript/14thOct.js
@@ -3,7 +3,7 @@ let sample_input_arr2 = [9,7,0,2,12,1,17];
 
 function maxConsecutiveGap(...arr){
     sorted_arr = arr.sort();
-    max_diff = 0;
+    max_diff = -1;
     for(var i=0;i<sorted_arr.length;i++){
         if(Math.abs(sorted_arr[i+1]-sorted_arr[i])>max_diff){
             max_diff = Math.abs(sorted_arr[i+1]-sorted_arr[i]);
