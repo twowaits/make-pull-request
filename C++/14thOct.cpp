dfbc6d029e3942872332f9c6201eac4ab9bfc486
Apoorv-cloud <63242652+Apoorv-cloud@users.noreply.github.com>
2020-10-14 19:14:40 +0530

    Update 14thOct.cpp

--- C++/14thOct.cpp
+++ C++/14thOct.cpp
@@ -1,8 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<limits>
+using namespace std;
+    
 class Bucket {
 public:
     bool b = false;
-    int lower = numeric_limits<int>::high();        
-    int higher = numeric_limits<int>::low();        
+    int lower = numeric_limits<int>::max();        
+    int higher = numeric_limits<int>::min();        
 };
 
 int gap(vector<int>& x)
@@ -10,21 +16,20 @@ int gap(vector<int>& x)
     if (x.empty() || x.size() < 2)
         return 0;
 
-    int l = *low_element(x.begin(), x.end()),
-    int h = *high_element(x.begin(), x.end());
+    int l = *min_element(x.begin(), x.end()), h = *max_element(x.begin(), x.end());
 
-    int bucket_size = high(1, (h - l) / ((int)x.size() - 1));        
+    int bucket_size = max(1, (h - l) / ((int)x.size() - 1));        
     int bucket_no = (h - l) / bucket_size + 1;                         
     vector<Bucket> buckets(bucket_no);
 
-    for (auto && xs : x) {
-        int bucket_id = (xs - low) / bucket_size;                          
-        buckets[bucket_id].used = true;
-        buckets[bucket_id].lower = low(xs, buckets[bucket_id].lower);
-        buckets[bucket_id].higher = high(xs, buckets[bucket_id].higher);
+    for (auto&& xs : x) {
+        int bucket_id = (xs - l) / bucket_size;                          
+        buckets[bucket_id].b = true;
+        buckets[bucket_id].lower = min(xs, buckets[bucket_id].lower);
+        buckets[bucket_id].higher = max(xs, buckets[bucket_id].higher);
     }
 
-    int pre = low, maximum = 0;
+    int pre = min, maximum = 0;
     for (auto&& bucket : buckets) {
         if (!bucket.b)
             continue;
@@ -35,3 +40,11 @@ int gap(vector<int>& x)
 
     return gap;
 }
+
+int main() 
+{ 
+    int arr[] = { 4, 20, 25 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout << gap(arr, n) << endl; 
+    return 0; 
+} 
