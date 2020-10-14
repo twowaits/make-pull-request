4bc460a202d381e76e15a046647d53fa8d0a05cc
gmohit688 <57702436+gmohit688@users.noreply.github.com>
2020-10-15 00:45:36 +0530

    Update 12thOct-17.java
    answer of the giver question with approach explained in below section.

--- JAVA/12thOct-17.java
+++ JAVA/12thOct-17.java
@@ -1,61 +1,50 @@
 import java.util.Scanner;
 class arrange
 {
-    
-    void Sort(int arr[])
-    {
-        int N=arr.length;
-        int l=0,m=0,h=N-1;
-        while(m<=h)
-        {
-            if(arr[m]==0)
-            {
-                int t=arr[l];
-                arr[l]=arr[m];
-                arr[m]=t;
-                l++;
-                m++;
-            }
-            else if(arr[m]==2)
-            {
-                int t=arr[m];
-                arr[m]=arr[h];
-                arr[h]=t;
-                h--;
-                m++;
-            }
-            else
-            {
-                m++;
-            }
-    }
-}
- 
-    void printArray(int A[])
-    {
-        int n = A.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(A[i] + " ");
-        System.out.println();
-    }
-    public static void main()
-    {   int p,m;
+    public static void main(String[] args)
+    {   int n;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of elements in array");
-        p=sc.nextInt();
-        int ar[] = new int[p];
-        for(m=0;m<p;m++)
+        n = sc.nextInt();
+        int ar[] = new int[n];
+        for(int i = 0; i < n; i++)
         {
-            System.out.println ("Enter an element :");
-            ar[m] = sc.nextInt();
+            ar[i] = sc.nextInt();
         }
-        arrange obj = new arrange();
-        obj.Sort(ar);
-        System.out.println("Sorted array :");
-        obj.printArray(ar);
-
+         int zero = 0, one = 0, two = 0;
+        for( int i = 0; i < n; i++){
+            if(ar[i] == 0){
+                zero++;
+            }
+            else if(ar[i] == 1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+         int count = 0;
+         while(count < zero){
+             ar[count] = 0;
+             count++;
+          }
+         one = zero+one;
+         while(count < one){
+             ar[count] = 1;
+             count++;
+          }
+         two += one;
+         while(count < two){
+             ar[count] = 2;
+             count++;
+         }
+         for( int i = 0 ; i < n; i++){
+             System.out.print(ar[i]+" ");
+         }
+         System.out.println();
     }
 }
 
-    
+    /*APPROACH: Implement the for loop through the array and count the frequencies of each distinct element(0 , 1 , 2) then simply change
+                the values of array according to frequencies. */
         
