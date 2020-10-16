918ee706315d97f1251d85ee4cf304216e2fd290
anishimishra <55035814+anishimishra@users.noreply.github.com>
2020-10-16 13:16:23 +0530

    Update 10thOct.java

--- JAVA/10thOct.java
+++ JAVA/10thOct.java
@@ -1,11 +1,8 @@
-import java.util.*;
-class String_Reverse
+import java.lang.*; //default package in java programming language
+class Demo
 {
-   public static void main (String[] args)
-   {
-      Scanner sc=new Scanner(System.in);
-      System.out.println("Enter a sentence");
-      StringBuilder sb=new StringBuilder(sc.nextLine());
-      System.out.println("Reversed String:- "+sb.reverse());
-   }
+	public static void main(String args[])
+	{
+		System.out.println("My first Java Program"); //out is an object, println is method of string class, system is class
+	}
 }
