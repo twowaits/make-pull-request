994fe61c906fc02d148bfda99c0f9c5f3f1483a0
anishimishra <55035814+anishimishra@users.noreply.github.com>
2020-10-16 13:23:08 +0530

    Update 10thoct1.py

--- Python/10thoct1.py
+++ Python/10thoct1.py
@@ -1,22 +1,17 @@
-l=[]
-real=[]
-while (True):
-    
-    userinput=input("enter your string\n")
-    
-    p=userinput[::-1]
-    
-    real.append(userinput)
-    l.append(p)
-    print(p)
-    print("press 1 if you want to exit or press 2 for continue ")
-    press=int(input())
-    if(press==1):
-        print("the real strings are!!\n")
-        print(real)
-        
-        print("the final reversed strings are\n")
-        print(l)
-        break
-    else:
-        q=p
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	now = datetime.datetime.now()
+	new_year = now.month == 1 and now.day == 1
+	#new_year = True #toh itll show true
+	return render_template("index2.html", new_year=new_year)
+
+
+#in line 9- i am defining a variable called now which is set to datetime.datetime.now which is pythons way of getting the current date&time
+#in line 10-i am defining a boolean variable called new_year which is going to be true if it is new years and its going to be false 
+#if its not if current date and month is 1 then its new years otherwise it is going to be false
