3bbbaa5d5bf222d05eead93342aa63fd826fa3d6
Maathavan <maathumaathavan@gmail.com>
2020-10-24 17:20:34 +0530

    Update 22ndOct-3.py

--- Python/22ndOct-3.py
+++ Python/22ndOct-3.py
@@ -1,11 +1,5 @@
 from itertools import permutations 
-import string 
-
 s = str(input())
-a = string.ascii_letters 
 p = permutations(s) 
-d = [] 
-for i in list(p): 
-    if (i not in d): 
-        d.append(i) 
+for i in list(p):
         print(''.join(i))
