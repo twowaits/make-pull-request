04555449e345c981c53a5888fbca6f4c3bd343c1
Prashanth Nair <54662001+9rstpn@users.noreply.github.com>
2020-10-30 15:07:21 +0530

    Update and rename 27thOct-1.java to 27thOct-4.java

---
+++ JAVA/27thOct-4.java
@@ -0,0 +1,29 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GenerateBNo
+{
+    static void generatePrintBinary(int n)
+    {
+        Queue<String> q = new LinkedList<String>(); 
+        q.add("1"); 
+          
+        while(n-- > 0) 
+        {
+            // print the front of queue 
+            String s1 = q.peek(); 
+            q.remove(); 
+            System.out.println(s1); 
+            String s2 = s1;
+            q.add(s1 + "0"); 
+            q.add(s2 + "1"); 
+        }
+    }
+	
+    public static void main(String[] args)
+    { 
+        int n=10; 
+        generatePrintBinary(n); 
+    }
+}
