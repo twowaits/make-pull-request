ea2da812d820be81f9dd55d37d8393db47a48f9b
Shivam kumar Jha <76209315+Shivamjha12@users.noreply.github.com>
2021-09-26 19:54:55 +0530

    Update 26thSep.java
    I hope this code working well, if there is an issue, please correct me. Thanks

--- JAVA/2021/26thSep.java
+++ JAVA/2021/26thSep.java
@@ -1,1 +1,28 @@
 // Add code here by achintya gaumat..
+
+// Java program to generate binary numbers from 1 to n
+ 
+import java.util.LinkedList;
+import java.util.Queue;
+ 
+public class 26thSep {
+    static void generatePrintBinary(int n)
+    {
+        Queue<String> q = new LinkedList<String>();
+ 
+        q.add("1");
+        while (n-- > 0) {
+            String s1 = q.peek();
+            q.remove();
+            System.out.println(s1);
+            String s2 = s1;
+            q.add(s1 + "0");
+            q.add(s2 + "1");
+        }
+    }
+    public static void main(String[] args)
+    {
+        int n = 10;
+        generatePrintBinary(n);
+    }
+}
