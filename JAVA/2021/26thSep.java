6deec7f8ee8c7f9cae81242760a41d56bb2ffa0a
Harsh Kumar <harsh9810kumar@gmail.com>
2021-09-26 21:42:57 +0530

    Update 26thSep.java
    I completed the task and code is running fluently. If you face any issue then please let me know.😊

--- JAVA/2021/26thSep.java
+++ JAVA/2021/26thSep.java
@@ -6,7 +6,7 @@ public class Decimal_To_Binary {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number you want to convert= ");
         int n=sc.nextInt();
-        for(int i = 0; i <= n; i++) {
+        for(int i = 0; i <= n; i++ ) {
             System.out.print(Integer.toBinaryString(i)+" ");
         }
     }
