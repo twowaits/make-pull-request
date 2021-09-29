b0f9a1bb594ec723855b350213c5f3b81407a7a1
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2021-09-29 22:11:07 +0530

    Create 29Sept_varsha080.cpp

---
+++ C++/2021/29Sept_varsha080.cpp
@@ -0,0 +1,55 @@
+// This code is contributed by Varsha Gupta
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+int main()
+{
+	//array declaration
+	int arr[MAX];
+	int n,i,j;
+	int temp;
+	
+	//read total number of elements to read
+	cout<<"Enter total number of elements to read: ";
+	cin>>n;
+	
+	//check bound
+	if(n<0 || n>MAX)
+	{
+		cout<<"Input valid range!!!"<<endl;
+		return -1;
+	}
+	
+	//read n elements
+	for(i=0;i<n;i++)
+	{
+		cout<<"Enter element ["<<i+1<<"] ";
+		cin>>arr[i];
+	}
+	
+	
+	//sorting - ASCENDING ORDER
+	for(i=0;i<n;i++)
+	{		
+		for(j=i+1;j<n;j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp  =arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	
+	//print sorted array elements
+	for(i=0;i<n;i++)
+		cout<<arr[i]<<" , ";
+	cout<<endl;	
+	
+	
+	return 0;
+	
+}
