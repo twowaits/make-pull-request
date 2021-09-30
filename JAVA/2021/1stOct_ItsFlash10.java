60e644c62def6f19c1db12fa005dbdfe812b632f
Satyam Shubham <57027429+ItsFlash10@users.noreply.github.com>
2021-10-01 01:22:38 +0530

    Java code with comments

--- JAVA/2021/1stOct_ItsFlash10.java
+++ JAVA/2021/1stOct_ItsFlash10.java
@@ -3,17 +3,20 @@ public class code {
     public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args){
         
+        //taking input from the user
         int n = scn.nextInt();
 
         int[] res = new int[n];
         res = fibOutput(n);
-
+        
+        //printing the required series
         for (int i = 0; i < n; i++) 
         {
             System.out.print(res[i] + " ");            
         }
     }
-
+    
+    //function to check prime
     public static boolean isPrime(int a) {  
         if (a <= 1) {  
             return false;  
@@ -25,7 +28,7 @@ public class code {
         }  
         return true;  
     }  
-
+    //function to get the series in an array
     public static int[] fibOutput(int n)
     {
         int a = 1;
@@ -34,10 +37,12 @@ public class code {
         int idx = 0;
 
         for(int i= 0; i<n; i++){
+            //checking if the number is divisible bt 5
             if(a % 5 == 0)
             {
                 res[idx++] = 0;
             }
+            //checking for prime
             else if(isPrime(a) == true)
             {
                 res[idx++] = 0;
@@ -46,7 +51,7 @@ public class code {
             {
                 res[idx++] = a;
             }
-            
+            //continuing the series further
             int c = a+b;
             a = b;
             b= c;
