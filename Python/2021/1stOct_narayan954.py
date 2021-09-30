3e013856f1dbcea899be30b4073889a0eca766a3
Ankur Singh <sin2ank84@gmail.com>
2021-10-01 01:15:35 +0530

    Fibbonaci by Recursion

--- Python/2021/1stOct_narayan954.py
+++ Python/2021/1stOct_narayan954.py
@@ -1,35 +1,15 @@
-def check_prime(num):
-    if num > 1:
-        for i in range(2, num // 2 + 1):
-            if (num % i) == 0:
-                return False
-        else:
-            return True
-    else:
-        return False
+def Fibonacci_series(Number):
+    if(Number == 0): #if value of n is 0 
+        return 0
+    elif(Number == 1): #if value of n is 1
+        return 1
+    else:#if value of n is greater than 1 it uses recursive calls
+        return (Fibonacci_series(Number - 2) + Fibonacci_series(Number - 1))
+    
+#taking input
+n = int(input("Enter the value of 'n': "))
+print("Fibonacci Series:", end = ' ')
 
-
-# Program to display the Fibonacci sequence up to nth term
-n = int(input("How many terms? "))
-
-n1, n2 = 1, 1  # first two terms
-count = 0
-
-# check if the number of terms is one
-if n == 1:
-    print("Fibonacci sequence upto", n, ":")
-    print(n1)
-
-# generate fibonacci sequence
-else:
-    print("Fibonacci sequence:")
-    while count < n:
-        if not check_prime(n1) and n1 % 5 != 0:
-            print(n1, end=' ')
-        else:
-            print(0, end=' ')
-        nth = n1 + n2
-        # update values
-        n1 = n2
-        n2 = nth
-        count += 1
+#printing output
+for n in range(0, n): 
+    print(Fibonacci_series(n), end = ' ')
