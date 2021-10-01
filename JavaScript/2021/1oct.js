e8ed8955d94719dda90be71201fcd8be0c855626
ADGAMERX <amank.u.m100@gmail.com>
2021-10-01 17:29:42 +0530

    added js code to reverse string

---
+++ JavaScript/2021/1oct.js
@@ -0,0 +1,20 @@
+//Javascript code to reverse string
+
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+ 
+reverseString("hello");
