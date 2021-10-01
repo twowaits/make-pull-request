326298fafeb6a5283f03dbb0236df858d3a07974
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-10-01 05:09:12 +0000

    solve java program.

---
+++ JAVA/2021/Oct1-saif-malik-01.java
@@ -0,0 +1,27 @@
+
+class Oct1-saif-malik-01{
+    public static void main(String[] args) {
+   
+      int n = 12;
+      int a = 1 ,b = 1;    
+      for(int i=0;i<n;i++){
+      if(b%5==0 || isPrime(b) ) System.out.print(0+" ");
+      else System.out.print(b+" ");
+      int temp = a;
+      a = a+b;
+      b = temp; 
+      }
+    }
+    
+    public static boolean isPrime(int number) {
+        if(number == 1) return false;
+        int sqrt = (int) Math.sqrt(number) + 1;
+        for (int i = 2; i < sqrt; i++) {
+            if (number % i == 0) {
+                // number is perfectly divisible - no prime
+                return false;
+            }
+        }
+        return true;
+    }
+}
