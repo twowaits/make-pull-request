86abd90bda25437f060677bd9e35402a3c04d43d
shreyas-9 <60311883+shreyas-9@users.noreply.github.com>
2021-10-01 20:20:34 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int n, num, digit, rev = 0;
+
+     cout << "Enter a positive number: ";
+     cin >> num;
+
+     n = num;
+
+     do
+     {
+         digit = num % 10;
+         rev = (rev * 10) + digit;
+         num = num / 10;
+     } while (num != 0);
+
+     if (n == rev)
+         cout << " The number is a palindrome.";
+     else
+         cout << " The number is not a palindrome.";
+
+    return 0;
+}
