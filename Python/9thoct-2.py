79c4285d8bf852054bdda87465dcd2dcd52afa84
Kapil Kungwani <68387141+kapilkungwani@users.noreply.github.com>
2021-10-01 19:26:21 +0530

    Create 1stOct_kapilkungwani.cpp

--- Python/9thoct-2.py
+++ Python/9thoct-2.py
@@ -1,20 +1,14 @@
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2nd number: "))
-i = 1
-while(i <= num1 and i <= num2):
-  if(num1 % i == 0 and num2 % i == 0):
-    gcd = i
-  i = i + 1
-print("GCD is", gcd)
+def fact(n):
+    if(n<1):
+        return 1
+    else:
+        return n*fact(n-1)
 
-#gcd of factorials of two number
+def main():
+    print("Enter number: ")
+    inp=input()
+    a,b=map(int,inp.split())
+    res=fact(min(a,b))
+    print(res)
 
-import math 
-  
-def gcdOfFactorial(m, n) : 
-    return math.factorial(min(m, n)) 
-  
-# Driver code 
-m = 5
-n = 9
-print(gcdOfFactorial(m, n))
+main()
