6440b8a5dd8002976bc8861386cd0bd7e978b4fb
Neo <91721577+via-lacteian@users.noreply.github.com>
2021-10-01 13:48:04 +0530

    Update 10thOct-1.py
    Added comments to understand the program better.

--- Python/10thOct-1.py
+++ Python/10thOct-1.py
@@ -1,7 +1,9 @@
-def rev(s):
-	str=" "
-	for i in s:
-		str=i+str
-	return str
-s=input("enter")
-print(rev(s))
+# function to reverse a given string
+def rev(s):                        	# Defining a new function
+	str=" "			   	# declaring an empty string to store reversed string
+	for i in s:		  	# assinging a for loop 
+		str=i+str	   	# storing each character one by one so the order gets reversed 
+	return str		  	# returning the new reversed string 
+s=input("Enter a string/n")	   	# asking input for an string to reverse
+print("The original string is: ",s)  
+print("The reverse string is",rev(s))	# displaying the revesed string 
