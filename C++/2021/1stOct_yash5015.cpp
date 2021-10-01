8df8c777d9496ef7fa59948c6450852ec02cbf6e
nikunj441 <75022173+nikunj441@users.noreply.github.com>
2021-10-01 21:53:01 +0530

    Update 1stOct_nikunj441.cpp
    I have written the code of  fibonacci series with a twist.

--- C++/2021/1stOct_yash5015.cpp
+++ C++/2021/1stOct_yash5015.cpp
@@ -1,63 +1,56 @@
-#include <bits/stdc++.h>
-#define FAST                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-
+#include<iostream>
 using namespace std;
-typedef long long ll;
-const int mod = 1e9 + 7;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef double ld;
-#define pb push_back
+//Function to check if a number is prime or not
+bool checkPrimeNumber(int x) {
 
-bool isPrime(int n)
-{
-    if (n <= 1)
+    // Corner case: 0 & 1 are not prime numbers
+    if (x <= 1)
         return false;
-    for (int i = 2; i <= sqrt(n); i++)
-        if (n % i == 0)
+
+    // Check from 2 to x-1
+    for (int i = 2; i < x; i++)
+        if (x % i == 0)
             return false;
 
     return true;
 }
 
-int fibonacci(int n)
-{
-    int a = 1, b = 1, c, i;
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-    {
+//Function to print fibonnaci series
+int printFibonacci(int x){
+	int a = 1, b = 1, c;
+
+    //Corner case: number of elements is 0
+	if( x == 0)
+		return 0;
+
+    //Corner case: number of elements is 1
+    else if ( x == 1 ){
         cout << "1 ";
         return 0;
     }
-    cout << "1 1 ";
-    for (i = 3; i <= n; i++)
-    {
+
+    //initial values
+    cout <<a<<" "<<b<<" ";
+	for(int i = 3; i <= x; i++){
         c = a + b;
-        if (c % 5 != 0 && !isPrime(c))
-            cout << c << " ";
+        //Condition check for twist
+        if ( (c % 5 != 0) && !checkPrimeNumber( c ))
+        cout <<c<<" ";
         else
-            cout << "0 ";
+        cout <<"0 ";
+        //updating values for next iteration
         a = b;
         b = c;
-    }
-    return b;
+	}
+	return b;
 }
 
-int main()
-{
-    FAST;
-    int n = 0;
-    cout << " Please enter the value of n : ";
-    cin >> n;
-    fibonacci(n);
+int main(){
+    int x;
+    cout << " Enter the value of n : ";
+    cin >> x;
+	printFibonacci(x);
+    cout<<"\n";
     return 0;
-}
+} 
