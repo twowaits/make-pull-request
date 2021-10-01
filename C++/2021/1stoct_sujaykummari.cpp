1447036539d54b9394586c456ff931f686718eac
Jishnu sujay kummari <sujaykummari06@gmail.com>
2021-10-01 14:43:56 +0530

    Update 1stoct_sujaykummari.cpp

--- C++/2021/1stoct_sujaykummari.cpp
+++ C++/2021/1stoct_sujaykummari.cpp
@@ -1,50 +1,70 @@
 //fibonachi series with a twist
 //done by sujaykummari
-
 #include<bits/stdc++.h>
+#define ps(x, y) fixed << setprecision(y) << x
+#define BOOST  ios_base::sync_with_stdio(false);    cin.tie(NULL);
+#define vi  vector<int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define rall(v) v.rbegin(), v.rend()
+#define next next_permutation
+#define perv prev_permutation
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define setbit(x) __builtin_popcount(x)
+#define w(t) while (t--)
+#define pq priority_queue
+
+#define FOR(i,a,b) for ( i = (a); i < (b); ++i)
+#define FORN(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for ( i = (b)-1; i >= (a); --i)
+#define ROFN(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
 using namespace std;
-void fib(int n)
-{
-    int temp=0,c=0;
-    int a=1;
-    int b[n];
-    b[0]=1;
-    for(int i=1;i<n;i++)
-    {
-        c=temp+a;
-        b[i]=c;
-        temp=a;
-        a=c;
-    }
-    int temp2=0;
-    for(int i=0;i<n;i++)
-    {
-        if(b[i]%5==0)
-            b[i]=0;
-        else
-            {
-                for(int j=1;j<=b[i];j++)
-                {
-                    if(b[i]%j==0)
-                    {
-                        temp2++;
-                    }
-                }
-                if(temp2==2)
-                    b[i]=0;
-                
-            }
-            temp2=0;
-    }
-    for(int i=0;i<n;i++)
-    {
-        cout<<b[i]<<" ";
-    }
-}
-int main()
-{
-    int N;
-    cin>>N;
-    fib(N);
-    return 0;
+#define int long long
+
+int mod = 1000000007;
+const int N = 1e5;
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("outputt.txt", "w", stdout);
+#endif
+
+	int n; cin >> n;
+
+	bool primes[N + 1];
+	memset(primes, true, N + 1);
+	primes[0] = false; primes[1] = false;
+
+
+	for (int i = 2; i * i <= N; i++) {   
+		if (primes[i]) {
+			for (int j = i * i; j <= N; j += i)
+				primes[j] = false;
+		}
+	}
+
+	int fib[n + 1];
+	fib[0] = 0;  fib[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		fib[i] = fib[i - 1] + fib[i - 2];
+
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (primes[fib[i]] || fib[i] % 5 == 0) cout << "0 ";
+		else cout << fib[i] << " ";
+	}
+
+	cout << endl;
+	return 0;
 }
