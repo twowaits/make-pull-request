c0b96604e4ff4f773140e17f96743a124d5fd41a
AnshDeep Yadav <59330323+AnshDeep24@users.noreply.github.com>
2021-10-01 10:32:18 +0530

    2 pr for hacktoberfest21

---
+++ Python/2021/1oct_ansh.py
@@ -0,0 +1,9 @@
+# Python program to print
+# ASCII Value of Character
+ 
+# In c we can assign different
+# characters of which we want ASCII value
+ 
+c = 'h'
+# print the ASCII value of assigned character in c
+print("The ASCII value of '" + c + "' is", ord(c))
