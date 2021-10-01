268fee45513f748fc3a26719be674f5422bce1a6
pranavhingankar <81463463+pranavhingankar@users.noreply.github.com>
2021-10-01 16:06:15 +0530

    created by pranavhingankar

--- C++/2021/1stOct_pranavhingankar.cpp
+++ C++/2021/1stOct_pranavhingankar.cpp
@@ -5,28 +5,28 @@
 
 using namespace std;
 
-bool isPrime(int number){
-    if(number == 1)
+bool Prime(int n){
+    if(n == 1)
         return false;
-    for(int divisor = 2;divisor*divisor <= number;divisor++){
-        if(number%divisor == 0)
+    for(int divisor = 2;divisor*divisor <= n;divisor++){
+        if(n%divisor == 0)
             return false;
     }
     return true;
 }
 
 int main(){
-    int number_of_terms;
-    cin>>number_of_terms;
-    long long int number1 = 1,number2 = 1,sumof2;
-    while(number_of_terms-- > 0){
-        if(number1%5 == 0 || isPrime(number1))
+    int nt;
+    cin>>nt;
+    long int n1 = 1,n2 = 1,s2;
+    while(nt-- > 0){
+        if(n1%5 == 0 || Prime(n1))
             cout<<0<<" ";
-        else
-            cout<<number1<<" ";
-        sumof2 = number1 + number2;
-        number1 = number2;
-        number2 = sumof2;
+    else
+            cout<<n1<<" ";
+    s2 = n1 + n2;
+    n1 = n2;
+   n2 = s2;
     }
     return 0;
 } 
