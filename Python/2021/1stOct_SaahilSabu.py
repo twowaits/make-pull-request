f047c54c541e3267fc8f03cf75b66831b24b6572
SAAHIL SABU HAMEED <saahil.sabu@gmail.com>
2021-10-01 12:57:02 +0530

    Add files via upload
    fibonacci with a twist using python

---
+++ Python/2021/1stOct_SaahilSabu.py
@@ -0,0 +1,29 @@
+def prime(n):
+    if n > 1:
+        for i in range(2, n // 2 + 1):
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+n = int(input())
+
+n1, n2 = 1, 1 
+count = 0
+
+
+if n == 1:
+    print(n1)
+
+else:
+    while count < n:
+        if not prime(n1) and n1 % 5 != 0:
+            print(n1, end=' ')
+        else:
+            print(0, end=' ')
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
