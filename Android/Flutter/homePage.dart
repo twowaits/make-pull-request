760529f4c2bd712c84c29f1d63b6119ff46bf140
Ali Ahmad Raza <52241717+raza4399@users.noreply.github.com>
2021-10-01 08:32:31 +0530

    added a basic homepage code for flutter development.

---
+++ Android/Flutter/homePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('it's home page'),
+        ),
+      ),
+    );
+  }
+}
