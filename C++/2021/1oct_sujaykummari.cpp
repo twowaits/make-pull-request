01dc8dd8e8398270ed884b244ee016bf6f0f0e85
Jishnu sujay kummari <sujaykummari06@gmail.com>
2021-10-01 12:59:47 +0530

    Create 1oct_sujaykummari.cpp

---
+++ C++/2021/1oct_sujaykummari.cpp
@@ -0,0 +1,50 @@
+//fibonachi series with a twist
+//done by sujaykummari
+
+#include<bits/stdc++.h>
+using namespace std;
+void fib(int n)
+{
+    int temp=0,c=0;
+    int a=1;
+    int b[n];
+    b[0]=1;
+    for(int i=1;i<n;i++)
+    {
+        c=temp+a;
+        b[i]=c;
+        temp=a;
+        a=c;
+    }
+    int temp2=0;
+    for(int i=0;i<n;i++)
+    {
+        if(b[i]%5==0)
+            b[i]=0;
+        else
+            {
+                for(int j=1;j<=b[i];j++)
+                {
+                    if(b[i]%j==0)
+                    {
+                        temp2++;
+                    }
+                }
+                if(temp2==2)
+                    b[i]=0;
+                
+            }
+            temp2=0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<b[i]<<" ";
+    }
+}
+int main()
+{
+    int N;
+    cin>>N;
+    fib(N);
+    return 0;
+}
