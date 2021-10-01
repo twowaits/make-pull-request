531ad189476a2c5c858ef94bc1e9444be8717edd
Shivam Patel <67095888+patelshivam3000@users.noreply.github.com>
2021-10-01 14:14:33 +0530

    Update Oct22-2.java

--- JAVA/Oct22-2.java
+++ JAVA/Oct22-2.java
@@ -1,25 +1,41 @@
-// Permutation Program
 
-public class Perm {
+public class Permutation
+{
+	public static void main(String[] args)
+	{
+		String str = "ABC";
+		int n = str.length();
+		Permutation permutation = new Permutation();
+		permutation.permute(str, 0, n-1);
+	}
+
 	
-	// Print all permutations as string
-	static void printPerm(String str, String ans) {
-
-		if (str.length() == 0) {
-			System.out.print(ans + " ");
-			return;
-		}
-
-		for (int i = 0; i < str.length(); i++) {
-
-			char h = str.charAt(i);
-			String r = str.substring(0, i) + str.substring(i + 1); 
-			printPerm(r, ans + h); 
+	private void permute(String str, int l, int r)
+	{
+		if (l == r)
+			System.out.println(str);
+		else
+		{
+			for (int i = l; i <= r; i++)
+			{
+				str = swap(str,l,i);
+				permute(str, l+1, r);
+				str = swap(str,l,i);
+			}
 		}
 	}
 
-	public static void main(String[] args) {
-	    String s = "AGSF";
-	    printPerm(s, "");
+	
+	public String swap(String a, int i, int j)
+	{
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i] ;
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
 	}
+
 }
+
+// This code is contributed by Mihir Joshi
