57a8445a29e4531d70fecc03e035ae84f5939aa3
Deepanshi <gargdeepanshi1509@gmail.com>
2021-10-01 23:17:59 +0530

    isPrime Solution Using DP

--- JAVA/1Oct_ayushsaxena119.java
+++ JAVA/1Oct_ayushsaxena119.java
@@ -28,14 +28,9 @@ class GFG {
 	}
 	
 	private static boolean isPrime(int n) {
-	    if (n == 1) {
-	        return false;
-	    }
-	    for (int i = 2; i * i <= n; i++) {
-	        if (n % i == 0) {
-	            return false;
-	        }
-	    }
-	    return true;
+	  for (int i=2; i<=n/2; i++) {
+            if (n%i==0) return false;
+        }
+        return true; 
 	}
 }
