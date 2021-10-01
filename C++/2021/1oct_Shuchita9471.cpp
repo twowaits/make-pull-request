1dc71f9be0ebf11fb6594c2e411ad051a4d7ed3e
Shuchita9471 <65860251+Shuchita9471@users.noreply.github.com>
2021-10-01 21:43:08 +0530

    Add files via upload

--- C++/2021/1oct_Shuchita9471.cpp
+++ C++/2021/1oct_Shuchita9471.cpp
@@ -1,37 +1,27 @@
 #include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-int P(int NM)
-{
-    if (NM <= 1)
-        return 0;
-    for (int i = 2; i < NM; i++)
-        if (NM % i == 0)
-            return 1;
-    return 0;
+
+int p(int n){
+    if(n<=1) return 0;
+    for(int i=2;i<n;i++){
+        if(n%i==0) return 1;
+    }return 0;
 }
-int main()
-{
-    int i, num, N1 = 0, N2 = 1;
-    int N3 = N1 + N2;
-    cin >> num;
-    cout << N2 << " ";
-    for (i = 3; i <= num + 1; ++i)       //if multiple of 5
-    {
-        if (N3 % 5 == 0)
-        {
-            cout << 0 << " ";
-        }
-        else if (P(N3) == 0)            //if prime
-        {
-            cout << "0" << " ";
-        }
-        else
-        {
-            cout << N3 << " ";
-        }
-        N1 = N2;
-        N2 = N3;
-        N3 = N2 + N1;
-    }
+
+int main() {
+	int i,n,n1=0,n2=1;
+	int n3=n1+n2;
+	cin>> n;
+	cout << n2<<" ";
+	for(i=3;i<=n+1;++i){
+	    if(n3%5==0) cout<< 0 << " ";
+	    else if(p(n3)==0) cout << 0 <<" ";
+	    else cout << n3 << " ";
+	    
+	    n1=n2;
+	    n2=n3;
+	    n3=n1+n2;
+	}
+	return 0;
 }
