2c9f8b0be4af3a01b801ee5f35c1a88a9dcd0fb3
Jobin S <63976083+Jobin-S@users.noreply.github.com>
2021-10-01 15:08:00 +0530

    Create jobinS.js

---
+++ JavaScript/jobinS.js
@@ -0,0 +1,14 @@
+const value = {
+  firstName: "JOBIN",
+  lastName: "S",
+  age:18,
+  school:"Shanthigiri Vidhyabhavan"
+};
+
+const {firstName, lastName, age, school} = value;
+
+console.log(`
+  My name is ${firstName} ${lastName}.
+  I am ${age} years old.
+  I am studying in ${school}
+`)
