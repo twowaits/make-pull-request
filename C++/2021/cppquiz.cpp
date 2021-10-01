385310beef468d349d5bc57afb72fd887d0209c6
Shrasti <shrastipachauri3@gmail.com>
2021-10-01 17:36:32 +0530

    Added Cpp quiz question

---
+++ C++/2021/cppquiz.cpp
@@ -0,0 +1,15 @@
+
+//printing with empty main
+#include <iostream>
+  
+class MyClass {
+public:
+    MyClass()
+    {
+        std::cout << "hello world";
+    }
+} m;//global object
+  
+int main()
+{
+}
