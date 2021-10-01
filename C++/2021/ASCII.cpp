6c4a793d3fd40b393894a7ef7632890d05bcbfb2
RootUserGit <79445341+RootUserGit@users.noreply.github.com>
2021-10-01 18:28:45 +0530

    Create ASCII.cpp

---
+++ C++/2021/ASCII.cpp
@@ -0,0 +1,16 @@
+//c++ Program to find the ASCII code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+    char ch;
+    cout<<"enter an alphabet = ";
+    cin>>ch;
+    cout<<"ASCII code for "<<ch<<" is = "<<int(ch);
+    return 0;
+}
+
+//Program created by RootUserGit
