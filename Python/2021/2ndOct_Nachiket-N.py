8c655a95818aab50932dd90d4e33378370ce9284
Nachiket-N <nachiket.n.naik@gmail.com>
2021-10-02 09:22:17 +0530

    second day's problem

--- Python/2021/2ndOct_Nachiket-N.py
+++ Python/2021/2ndOct_Nachiket-N.py
@@ -1,42 +1,11 @@
-def numInRow(i, rows, start, columns):
-    arr = [start]
-    switch = False
-    while True:
-        if rows != i or rows != 0:
-            if switch:
-                ele = arr[-1] + 2*(i)
-            else:
-                ele = arr[-1] + 2*(rows-i-1)
-            switch = not switch
-        else:
-            ele = arr[-1] + 2*(rows-1)
-
-        if ele >= columns:
-            break
-        else:
-            arr.append(ele)
-    return sorted(list(set(arr)))
-
-def spaceBetween(a, b):
-    print(' '*(b-a), end="")
-
-def solve(rows, columns):
-    for i in range(rows):
-        spaceBetween(0, i)
-        elements = numInRow(i,rows,i,columns+1)
-        for j in range(len(elements)):
-            if len(str(elements[j])) > 1:
-                inend = ""
-            else:
-                inend = " "
-            print(elements[j], end=inend)
-            try:
-                spaceBetween(elements[j], elements[j+1])
-            except:
-                pass
-        print() 
-
-inp = input().split(" ")
-rows = int(inp[0])
-columns = int(inp[1])
-solve(rows, columns)
+rows,columns=list(map(int,input().split()))
+for i in range(1,rows+1):
+	print(" "*(i-1),end="")
+	x=i
+	while x<=columns:
+		print(x,end="")
+		v2=x%(rows-1)
+		if(y==0): y=(rows-1)
+		x+=(rows-y)*2
+		print(" "*((rows-y)*2-1),end="")
+	print("\n")
