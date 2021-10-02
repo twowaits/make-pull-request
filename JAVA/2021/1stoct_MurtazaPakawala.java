a1fea3ecadcc5d2d58394ebd7f66401aa3844e34
Murtaza Pakawala <mypaka52@gmail.com>
2021-10-02 19:41:06 +0530

    have made changes for better user readablity

--- JAVA/2021/1stoct_MurtazaPakawala.java
+++ JAVA/2021/1stoct_MurtazaPakawala.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class  twistfibanocci{
+  
   static boolean isPrime(int n)
     {
         
@@ -14,6 +15,8 @@ public class  twistfibanocci{
         return true;
     }
     public static void main(String[] args){
+      // taking input
+      System.out.println("please enter the number till fibonnaci series has to be printed");
       Scanner sc=new Scanner(System.in);
       int n ,sum, x,y;
       x=1;
@@ -22,10 +25,12 @@ public class  twistfibanocci{
       System.out.print(x+" "+y+" ");
       for(int i=2;i<n;i++)
       {
+        // calculating the sum of the number before this number "i"
         sum=x+y;
-        if(sum%5==0) System.out.print(0+" ");
-        else if(isPrime(sum)) System.out.print(0+" ");
-        else System.out.print(sum+" ");
+        if(sum%5==0) System.out.print(0+" ");//if sum is devisible by 5 printing 0
+        else if(isPrime(sum)) System.out.print(0+" ");//if sum is a prime number printing zero
+        else System.out.print(sum+" ");//if not any of the above condition printing the correct output
+        //changing the values of x and y to proceed for next iteration
         x=y;
         y=sum;
 
