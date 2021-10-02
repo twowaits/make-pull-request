30375dda66ca7b974b2225ab93fd51860eed03e2
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-02 16:51:24 +0530

    create a file named as 2ndOct_kritika-sharma130.py to create a program to display the respective pattern ZigZag Pattern.

---
+++ Python/2021/2ndOct_kritika-sharma130.py
@@ -0,0 +1,13 @@
+#The code given below depicts the code for the ZigZag Pattern
+
+centre_element, end_element = list(map(int, input().split()))
+for iterator in range(1, centre_element + 1):
+    print("  " * (iterator - 1), end="")
+    it = iterator
+    while it <= end_element:
+        print(it, end="")
+        temp = it % (centre_element - 1)
+        if (temp == 0): temp = (centre_element - 1)
+        it += (centre_element - temp) * 2
+        print("  " * ((centre_element - temp) * 2 - 1), end="")
+    print()
