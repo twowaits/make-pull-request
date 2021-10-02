0c27469539c239ce106be9fbfca4a73c5c9941dd
Aman Kuliyal <1devyansh1@gmail.com>
2021-10-02 06:58:38 +0530

    Create 2ndOct_Iconic-It-Is.cpp
    Resolved CPP issue for 2nd oct

---
+++ C++/2021/2ndOct_Iconic-It-Is.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+int main()
+{
+	int n, m,
+		i = 1;
+	std::cout<<"Enter Rows: ";
+	std::cin>>n;
+	std::cout<<"Enter Columns: ";
+	std::cin>>m;
+	while(i<=n)
+	{
+		int j = i-1;
+		while(j--)
+		{
+			std::cout<<" ";
+			}
+		std::cout<<i;
+		j = 2*(n-i);
+		int t = j;
+		while(t--)
+		{
+			std::cout<<" ";
+			}
+		if(j && i+j<=m) std::cout<<i+j;
+		int k = (i!=1) ? 2*(i-1): 0;
+		while(k-- && k)
+		{
+			std::cout<<" ";
+			}
+		int tmp = j+i+(i-1)*2;
+		if(tmp <= m && tmp != i+j)
+		std::cout<<tmp;
+		std::cout<<std::endl;
+		i++;
+		}
+	return 0;
+}
+/*
+output:-
+Enter Rows: 9
+Enter Columns: 15
+1                
+ 2               
+  3            15   
+   4          14     
+    5        13       
+     6      12         
+      7    11           
+       8  10             
+        9
+ */
