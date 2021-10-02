0b09a6c950289a08bb1566754ea942b4baa7ece9
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-10-02 11:20:25 +0530

    Created 02_October_Mohit.cpp

---
+++ C++/02_October_Mohit.cpp
@@ -0,0 +1,66 @@
+/**
+ *    author:  immadmohit      
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ff first
+#define ss second
+#define vec vector
+#define pb push_back
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define pi pair<int,int>
+#define mod(a,b) (a%b+b)%b
+#define ceil(a,b) ((a+b-1)/b)
+#define flushout cout.flush();
+#define sz(x) ((int) (x).size())
+#define __lcm(a,b) (a*b)/__gcd(a,b)
+#define all(x) (x).begin(), (x).end()
+#define vin(a) for(auto &ii : a)cin>>ii
+#define f(i,a,b) for(int i=(a);i<(b);i++)
+#define fr(i,a,b) for(int i=(b-1);i>=(a);i--)
+#define dbg(x) cout<<'>'<< #x <<':'<< x <<endl;
+#define pout(x,n) cout<<fixed<<setprecision(x)<<n
+#define vout(v) for(auto ii:v)cout<<ii<<' ';cout<<endl;
+const int M = 1000000007;
+
+/* CODE STARTS HERE */
+
+void init_code(){
+	#ifndef Mohit_Mishra
+    	freopen("Input.txt", "r", stdin);
+    	freopen("Output.txt", "w", stdout);
+    	freopen("Error.txt", "w", stderr);
+    #endif
+}
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	init_code();
+
+	string a;
+	cin >> a;
+
+	string b;
+
+	int t = 0;
+
+	for(int i = 0 ;i < 5 ;i++){
+		cin >> b;
+		if((b[0] == a[0]) or (b[1] == a[1])){
+			t++;
+		}
+	}
+	if(t > 0){
+		cout << "YES" << endl;
+	}
+	else
+		cout << "NO" << endl;
+	
+}
