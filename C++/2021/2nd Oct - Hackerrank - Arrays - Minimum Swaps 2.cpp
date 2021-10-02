06acdf8aedfa5a73bdf27cfabcd7c5a004f00af5
Saurav Soni <58345727+cris-ronaldo7@users.noreply.github.com>
2021-10-02 06:54:56 +0530

    Create 2nd Oct - Hackerrank - Arrays - Minimum Swaps 2.cpp
    Please add hacktoberfest-accepted label also.

---
+++ C++/2021/2nd Oct - Hackerrank - Arrays - Minimum Swaps 2.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+int answer=0;
+int i=0,j;
+for(i=0;i<arr.size();i++)
+{
+    if(arr[i]!=(i+1))
+    {
+        swap(arr[i],arr[arr[i]-1]);
+    answer++;
+    i--;
+    }
+    
+    
+
+    /*for(j=0;j<arr.size();j++)
+    cout<<arr[j]<<" ";
+    cout<<endl;*/
+}
+return answer;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
