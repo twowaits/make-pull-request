c5fd5e18cd5803e6f78a6055702dc269725f7329
raghuSpark <sparkraghu1508@gmail.com>
2021-10-02 12:08:32 +0530

    Solved ZigZag Problem in c++

---
+++ C++/2021/2ndOct_raghuSpark.cpp
@@ -0,0 +1,38 @@
+// ZIG-ZAG PROBLEM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int rows, columns;
+    cin >> rows >> columns;
+    if (rows == 1)
+    {
+        for (int c = 1; c <= columns; c++)
+            cout << c << " ";
+        cout << "\n";
+    }
+    else
+    {
+        for (int r = 1; r <= rows; r++)
+        {
+            for (int c = 1; c <= columns; c++)
+            {
+                if (c % (2 * rows - 2) == r or ((2 * rows - c) % (2 * rows - 2) == r % (2 * rows - 2)))
+                    cout << c;
+                else
+                {
+                    int temp = c;
+                    while (temp != 0)
+                    {
+                        cout << " ";
+                        temp /= 10;
+                    }
+                }
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
