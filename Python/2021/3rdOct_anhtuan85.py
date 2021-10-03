197d56b10b007457e9715a9d5c3dc0346407b524
anhtuan85 <edxmembers@gmail.com>
2021-10-03 10:13:06 +0700

    3rdOctober python isunique_chars

---
+++ Python/2021/3rdOct_anhtuan85.py
@@ -0,0 +1,53 @@
+import time
+import unittest
+from collections import defaultdict
+
+
+def is_unique_chars_algorithmic(string):
+    if len(string) > 128:    #Assume ASCII
+        return False
+
+    charset = [False] * 128
+    for char in string:
+        val = ord(char)
+        if charset[val]:
+            return False
+        charset[val] = True
+    return True
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ("abcd", True),
+        ("s4fad", True),
+        ("", True),
+        ("23ds2", False),
+        ("hb 627jh=j ()", False),
+        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
+    ]
+    test_functions = [
+        is_unique_chars_algorithmic,
+    ]
+
+    def test_is_unique_chars(self):
+        num_runs = 1000
+        function_runtimes = defaultdict(float)
+
+        for _ in range(num_runs):
+            for text, expected in self.test_cases:
+                for is_unique_chars in self.test_functions:
+                    start = time.perf_counter()
+                    assert (
+                        is_unique_chars(text) == expected
+                    ), f"{is_unique_chars.__name__} failed for value: {text}"
+                    function_runtimes[is_unique_chars.__name__] += (
+                        time.perf_counter() - start
+                    ) * 1000
+
+        print(f"\n{num_runs} runs")
+        for function_name, runtime in function_runtimes.items():
+            print(f"{function_name}: {runtime:.1f}ms")
+
+
+if __name__ == "__main__":
+    unittest.main()
