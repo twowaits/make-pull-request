9e03278d4e212e253b25166632efaf61e62c5cc0
Suraj-Wakka <58338343+surajwakka@users.noreply.github.com>
2021-10-09 12:19:06 +0530

    Create 9oct_surajwakka.cpp

---
+++ C++/2021/9oct_surajwakka.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int x)
+{
+	if (x <= 1)
+			return 1;
+				int res = 2;
+					for (int i = 3; i <= x; i++)
+							res = res * i;
+								return res;
+								}
+int gcdOfFactorial(int m, int n)
+{
+	return factorial(min(m, n));
+	}
+int main()
+{
+	int m = 5, n = 9;
+		cout << gcdOfFactorial(m, n);
+			return 0;
+			}
+			
