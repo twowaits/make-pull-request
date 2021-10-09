4c0fd46f3c195a08039b192a5380d1d25857e45a
Pooja-Das <81691805+Pooja-Das@users.noreply.github.com>
2021-10-09 13:06:39 +0530

    added GCD of two numbers in python

---
+++ Python/2021/9thSep_Pooja-Das.py
@@ -0,0 +1,9 @@
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+GCD=gcd(a,b)
+print(GCD)
