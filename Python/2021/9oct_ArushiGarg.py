891fe8e69a5a98d8e01f23edb998b2c68881c75b
072arushi <87575658+072arushi@users.noreply.github.com>
2021-10-09 17:53:36 +0530

    Create 9oct_ArushiGarg.py

---
+++ Python/2021/9oct_ArushiGarg.py
@@ -0,0 +1,26 @@
+n = int(input())
+n1, n2,fib = 1, 1, 0,
+
+def primeCheck(n):
+    if n > 1:
+        for i in range(2, n // 2 + 1):
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+if n == 1:
+    print(n1)
+
+else:
+    while fib < n:
+        if not primeCheck(n1) and n1 % 5 != 0:
+            print(n1, end=' ')
+        else:
+            print(0, end=' ')
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        fib += 1
