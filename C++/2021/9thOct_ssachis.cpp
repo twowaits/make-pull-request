68f3df6b3c48b1950ff3b9de4dee07d0e732f0a9
ssachis <89895559+ssachis@users.noreply.github.com>
2021-10-09 13:36:50 +0530

    Update 9thOct_ssachis.cpp

--- C++/2021/9thOct_ssachis.cpp
+++ C++/2021/9thOct_ssachis.cpp
@@ -2,7 +2,13 @@
 #include<iostream>
 using namespace std;
 int factorial(int x){
-
+      if(x<0){
+        return 0;
+    }
+    if(x==0){
+        return 1;
+    }
+ 
     int fact=1;
 for(int i=1;i<=x;i++){
     fact=fact*i;
