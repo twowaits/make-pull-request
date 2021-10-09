70a0fcb78e66b94bf73a5750919a7c188d282d33
ujjwalhans <56391241+ujjwalhans@users.noreply.github.com>
2021-10-09 15:29:33 +0530

    Create 9thOct_ujjwalhans.py

---
+++ Python/9thOct_ujjwalhans.py
@@ -0,0 +1,12 @@
+def gcd(a,b):
+    if (b == 0):
+        return a
+    if (a == 0):
+        return b
+    if (a == b):
+        return a
+    if (a > b):
+        return gcd(a-b, b)
+    return gcd(a, b-a)
+ans=gcd(5,10)
+print(ans)
