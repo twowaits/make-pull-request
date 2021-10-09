94beb74ed4c5f08d2672cdc924ffebe37b10b77c
jashpal2003 <60477148+jashpal2003@users.noreply.github.com>
2021-10-09 22:28:13 +0530

    Create 9thOct_jashpal2003.c

---
+++ C++/2021/9thOct_jashpal2003.c
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int x)
+{
+	if (x <= 1)
+		return 1;
+	int res = 2;
+	for (int i = 3; i <= x; i++)
+		res = res * i;
+	return res;
+}
+
+int gcdOfFactorial(int m, int n)
+{
+	return factorial(min(m, n));
+}
+
+int main()
+{
+	int m,n;
+    cin>>m;
+    cin>>n;
+	cout << gcdOfFactorial(m, n);
+	return 0;
+}
