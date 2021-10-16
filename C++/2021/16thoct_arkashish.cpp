3ac1db99ac2c62c5546833d8d3decae13300ac32
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-16 13:01:01 +0530

    Code for GCD of factorials added

---
+++ C++/2021/16thoct_arkashish.cpp
@@ -0,0 +1,28 @@
+//GCD of Factorials
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n)
+{
+    if (n <= 1)
+        return 1;
+    int ans = 2;
+    for (int i = 3; i <= n; i++)
+        ans = ans * i;
+    return ans;
+}
+
+int checkmin(int m, int n)
+{
+    return fact(min(m, n));
+}
+
+int main()
+{
+    int x,y;
+    cout<<"Enter the values of x and y: ";
+    cin >> x >> y;
+    cout << "The GCD of factorials is: " << checkmin(x, y) << endl;
+    return 0;
+}
