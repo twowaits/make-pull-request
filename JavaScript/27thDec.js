1d5785ce0678806b7679b9fc53283b0728e56ae0
Jaden Stock <jadenstock@hotmail.com>
2022-12-27 01:15:04 +0200

    Added a file that draws a rectangle

---
+++ JavaScript/27thDec.js
@@ -0,0 +1,7 @@
+// Draw a rectangle
+const width = 10
+console.log("*".repeat(width))
+for (let i = 0; i < width - 2; i++) {
+	console.log("*" + " ".repeat(width - 2) + "*")
+}
+console.log("*".repeat(width))
